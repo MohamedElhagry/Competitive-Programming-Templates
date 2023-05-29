aa4015081dfd33b81c116e6fad11099ce3c1883d
MohamedElhagry <85036940+MohamedElhagry@users.noreply.github.com>
2023-05-29 17:07:30 +0300

    Updated nCr, precomputed facts to reach O(1) complexity on query

--- nCr.cpp
+++ nCr.cpp
@@ -1,32 +1,39 @@
 
-ll fact[N];
-
-void preprocess()
-{
-    fact[0] = 1;
-    for(ll i=1; i<N; i++)
-        fact[i] = i * fact[i-1] % mod;
-}
+ll fact[N], modInv[N];
 
 ll fastExp(ll x, ll n)
 {
     if(n == 0)
         return 1;
     ll u = fastExp(x, n/2);
-    u = u * u % mod;
+    u = u*u % mod;
     if(n & 1)
         u = u * x % mod;
     return u;
 }
 
-ll modInv(ll x)
+// modInv[i] = fact[i]^-1 % mod
+void preprocess()
 {
-    return fastExp(x, mod-2) % mod;
+    fact[0] = 1;
+    for(ll i =1; i<N; i++)
+        fact[i] = fact[i-1] * i % mod;
+
+    modInv[N-1] = fastExp(fact[N-1], mod-2) % mod;
+    for(ll i=N-2; i>=0;i--)
+        modInv[i] = (i+1) * modInv[i+1] % mod;
+}
+
+ll modInvF(ll x)
+{
+    return fastExp(x, mod-2);
 }
 
 ll nCr(int n, int r)
 {
-    if(n < r)
+    if(r > n)
         return 0;
-    return (fact[n] * modInv(fact[r]) % mod) * modInv(fact[n-r]) % mod;
+
+    // return ( n! / ((n-r)! * r!) ) % mod
+    return (fact[n] * modInv[n-r] % mod) * modInv[r] % mod;
 }
